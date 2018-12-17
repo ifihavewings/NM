3d29a9d162952adced6f1b2332a1209cd975af71
sv <615999773@qq.com>
2018-12-17 21:25:09 +0800

    codewars toJadenCase

---
+++ codewars/1.toJadenCase.js
@@ -0,0 +1,16 @@
+// my version
+String.prototype.toJadenCase = function () {
+  return this.split(" ").map(item=> {return item.substring(0,1).toUpperCase() + item.substring(1)}).join(" ")
+};
+
+
+// good version 
+String.prototype.toJadenCase = function () {
+  return this.replace(/(^| )(\w)/g,  s=> { return s.toUpperCase(); });
+};
+let str = "hello Jon, I am your fan"
+str.toJadenCase()
+
+
+//  Summary
+// 字符串操作，尽量用正则或String的方法
